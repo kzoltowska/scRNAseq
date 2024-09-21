3b103818668c55547d398e64e3ffe21dbd1416ec
kzoltowska <k.zoltowska@oxfordalumni.org>
2024-09-21 18:54:41 +0200

    Creating boolean low feature column in metadata

--- scripts/scRNAseq_data_processing.R
+++ scripts/scRNAseq_data_processing.R
@@ -30,9 +30,16 @@ df_mitochondrial$Condition<-as.factor(df_mitochondrial$Condition)
 ggplot(df_mitochondrial, aes(x=Condition, y=mitochondrial)) +geom_violin(fill="cornflowerblue") + theme_pubr()+
   ylab("% mitochondrial transcript")
 
-#Filtering cells with high percentage (>25%) of mitochondrial transcripts
+#Filtering cells with high percentage (>25%) of mitochondrial transcripts----
 
 se_list_filtered<-lapply(se_list, function(x){
   x<-subset(x,mitochondrial < 25)
+  return(x)
+})
+
+#Cells with low feature count can cluster differently thus it is worth assigning them to a seperate category for visualisation ----
+#Here low feature is considered as 300 - note that while making Seurat object 200 was considered as min.features
+se_list_filtered<-lapply(se_list_filtered, function(x){
+  x$low_feature<-x$nFeature_RNA<300
   return(x)
 })
