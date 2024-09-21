ddb76896c8132e6929b1d9934ae30d2d8d49470c
kzoltowska <k.zoltowska@oxfordalumni.org>
2024-09-21 20:43:49 +0200

    Nearest neighbours, clustering, markers

--- scripts/scRNAseq_data_processing.R
+++ scripts/scRNAseq_data_processing.R
@@ -4,7 +4,7 @@ library(ggplot2)
 library(patchwork)
 library(ggpubr)
 library(dplyr)
-
+library(clustree)
 #Loading data & building Seurat objects ----
 
 mutant<-Read10X("~/Single_cell_RNAseq_scripts/data/MUT/")
@@ -32,14 +32,14 @@ ggplot(df_mitochondrial, aes(x=Condition, y=mitochondrial)) +geom_violin(fill="c
 
 #Filtering cells with high percentage (>25%) of mitochondrial transcripts----
 
-se_list_filtered<-lapply(se_list, function(x){
+se_list<-lapply(se_list, function(x){
   x<-subset(x,mitochondrial < 25)
   return(x)
 })
 
 #Cells with low feature count can cluster differently thus it is worth assigning them to a seperate category for visualisation ----
 #Here low feature is considered as 300 - note that while making Seurat object 200 was considered as min.features
-se_list_filtered<-lapply(se_list_filtered, function(x){
+se_list<-lapply(se_list, function(x){
   x$low_feature<-x$nFeature_RNA<300
   return(x)
 })
@@ -47,7 +47,7 @@ se_list_filtered<-lapply(se_list_filtered, function(x){
 #Data preprocessing - normalisation, finding variable features and scaling----
 #Normalisation:“LogNormalize”: Feature counts for each cell are divided by the total counts for that cell and multiplied by the scale.factor. 
 #This is then natural-log transformed using log1p
-
+se_list_filtered<-se_list
 se_list_filtered<-lapply(se_list_filtered, NormalizeData)
 
 #Find variable features----
@@ -76,3 +76,81 @@ lapply(se_list_filtered, function(x){
 lapply(se_list_filtered, ElbowPlot)
 
 #Considering the output of the heatmap and elbow plot 14 PCs will be used further
+#Next step aims at finding nearest neighbours
+
+se_list_filtered<-lapply(se_list_filtered, function(x){
+  x<-FindNeighbors(x, dims=1:14, graph.name=c("RNA_nn", "RNA_snn"))
+})
+
+#Finding clusters
+#Identify clusters of cells by a shared nearest neighbor (SNN) modularity optimization based clustering algorithm.
+se_list_filtered<-lapply(se_list_filtered, function(x){
+  x<-FindClusters(x, resolution=0.8, graph.name="RNA_snn")
+  x<-FindClusters(x, resolution=0.4, graph.name="RNA_snn")
+  x<-FindClusters(x, resolution=0.2, graph.name="RNA_snn")
+  x<-FindClusters(x, resolution=0.1, graph.name="RNA_snn")
+})
+
+#looking at clustering based on resolution
+#clustree gives nice visual insight how the assignment to clusters change based on resolution
+lapply(se_list_filtered, function(x){
+  clustree(x@meta.data, prefix="RNA_snn_res.")
+}
+)
+
+#calculate UMAP and plot clusters ----
+#it is worth taking a look at different resolutions
+se_list_filtered<-lapply(se_list_filtered, function(x){
+  x<-RunUMAP(x, dims=1:14, n.neighbors=20)
+})
+
+lapply(se_list_filtered,function(x){
+DimPlot(x, reduction="umap", group.by = "RNA_snn_res.0.4")
+}
+)
+
+#merge the data into single merged suerat object and re-perform the analysis
+#perform all the steps as before but now on merge
+SOM<-merge(se_list$WT, se_list$Mutant,
+           add.cell.ids=c(1,2))
+
+SOM<-NormalizeData(SOM)
+
+SOM <- FindVariableFeatures(SOM)
+
+SOM <- ScaleData(SOM)
+
+SOM<-RunPCA(SOM)
+
+DimHeatmap(SOM, dims=1:15, cells=500)
+ 
+SOM<-FindNeighbors(SOM, dims=1:14, graph.name = c("nn_som", "snn_som"))
+
+SOM<-FindClusters(SOM, graph.name = "snn_som", resolution = 0.1)
+SOM<-FindClusters(SOM, graph.name = "snn_som", resolution = 0.2)
+SOM<-FindClusters(SOM, graph.name = "snn_som", resolution = 0.4)
+SOM<-FindClusters(SOM, graph.name = "snn_som", resolution = 0.8)
+
+clustree(SOM, prefix="snn_som_res.")
+
+SOM<-RunUMAP(SOM, dims=1:14)
+
+DimPlot(SOM, reduction = "umap", group.by = "Condition")
+DimPlot(SOM, reduction = "umap", group.by = "snn_som_res.0.8")
+DimPlot(SOM, reduction = "umap", group.by = "snn_som_res.0.4")
+DimPlot(SOM, reduction = "umap", group.by = "snn_som_res.0.2")
+DimPlot(SOM, reduction = "umap", group.by = "snn_som_res.0.1")
+
+#Clusters need to be assigned before proceeding----
+Idents(SOM)<-SOM$snn_som_res.0.1
+
+#find markers ----
+markers<-JoinLayers(SOM)
+markers<-FindAllMarkers(markers, min.diff.pct = 0.5, only.pos = TRUE,
+                 max.cells.per.ident = 100)
+
+#Look at the markers using dotplot ----
+genes <- slice_head(group_by(markers,cluster),n=3)$gene
+DotPlot(SOM,features=genes,group.by="snn_som_res.0.1",
+        dot.scale=4,cols="RdYlBu") +
+  theme(axis.text.x=element_text(angle=45,hjust=1))
